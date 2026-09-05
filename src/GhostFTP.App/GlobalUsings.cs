c9ffb5b8af281c917ba18642a2c7c936edb37fdd
Brendigo <info@brendigo.com>
2026-09-05 22:22:34 +0200

    Alias shared cross-platform app data types in Windows renderer

--- src/GhostFTP.App/GlobalUsings.cs
+++ src/GhostFTP.App/GlobalUsings.cs
@@ -1,1 +1,5 @@
 global using System.IO;
+global using AppPaths = GhostFTP.Core.Services.AppPaths;
+global using AppSettings = GhostFTP.Core.Services.AppSettings;
+global using AppSettingsStore = GhostFTP.Core.Services.AppSettingsStore;
+global using AppTheme = GhostFTP.Core.Services.AppTheme;
